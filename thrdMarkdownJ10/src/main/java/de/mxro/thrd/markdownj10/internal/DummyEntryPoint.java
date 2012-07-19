f654217390230b6dea21be7c2ccce4a9fe576a7a
mroh004 <mroh004@dev>
2012-07-20 09:52:00 +1200

    Tried to add gwt compatability, but did not work since markdownj uses Java regexp

---
+++ thrdMarkdownJ10/src/main/java/de/mxro/thrd/markdownj10/internal/DummyEntryPoint.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright 2011 Max Erik Rohde http://www.mxro.de
+ * 
+ * All rights reserved.
+ ******************************************************************************/
+package de.mxro.thrd.markdownj10.internal;
+
+/**
+ * Necessary for GWT 2.X to compile this module
+ * 
+ * @author Max
+ *
+ */
+public class DummyEntryPoint {
+
+	public void onModuleLoad() {
+
+	}
+}
