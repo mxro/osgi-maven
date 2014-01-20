7c861d07638381ddace3053e452694e655cda7fe
mxro <mxro@nowhere.com>
2014-01-20 18:02:48 +1300

    Working on new GWT RPC implementation Autocommit Mon Jan 20 18:02:48 NZDT 2014

--- thrdNetty4/src/test/java/de/mxro/thrd/netty4/tests/TestNetty4.java
+++ thrdNetty4/src/test/java/de/mxro/thrd/netty4/tests/TestNetty4.java
@@ -1,5 +1,16 @@
 package de.mxro.thrd.netty4.tests;
 
+import org.junit.Test;
+
+import de.mxro.thrd.netty4.tests.http.HttpHelloWorldServer;
+
 public class TestNetty4 {
 
+	@Test
+	public void test_netty4_http_server() {
+		
+		HttpHelloWorldServer server = new HttpHelloWorldServer(18912);
+		
+	}
+	
 }
