3388eaf4314387be877bbb24076e73680254d95c
mxro <mxro@nowhere.com>
2014-01-20 18:03:48 +1300

    Working on new GWT RPC implementation Autocommit Mon Jan 20 18:03:48 NZDT 2014

--- thrdNetty4/src/test/java/de/mxro/thrd/netty4/tests/http/HttpHelloWorldServer.java
+++ thrdNetty4/src/test/java/de/mxro/thrd/netty4/tests/http/HttpHelloWorldServer.java
@@ -22,42 +22,40 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-
-
 public class HttpHelloWorldServer {
 
-	 private final int port;
+	private final int port;
 
-	    public HttpHelloWorldServer(int port) {
-	        this.port = port;
-	    }
+	public HttpHelloWorldServer(int port) {
+		this.port = port;
+	}
 
-	    public void run() throws Exception {
-	        // Configure the server.
-	        EventLoopGroup bossGroup = new NioEventLoopGroup();
-	        EventLoopGroup workerGroup = new NioEventLoopGroup();
-	        try {
-	            ServerBootstrap b = new ServerBootstrap();
-	            b.option(ChannelOption.SO_BACKLOG, 1024);
-	            b.group(bossGroup, workerGroup)
-	             .channel(NioServerSocketChannel.class)
-	             .childHandler(new HttpHelloWorldServerInitializer());
+	public void run() throws Exception {
+		// Configure the server.
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.option(ChannelOption.SO_BACKLOG, 1024);
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.childHandler(new HttpHelloWorldServerInitializer());
 
-	            Channel ch = b.bind(port).sync().channel();
-	            ch.closeFuture().sync();
-	        } finally {
-	            bossGroup.shutdownGracefully();
-	            workerGroup.shutdownGracefully();
-	        }
-	    }
+			Channel ch = b.bind(port).sync().channel();
+			ch.closeFuture().sync();
+		} finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
 
-	    public static void main(String[] args) throws Exception {
-	        int port;
-	        if (args.length > 0) {
-	            port = Integer.parseInt(args[0]);
-	        } else {
-	            port = 8080;
-	        }
-	        new HttpHelloWorldServer(port).run();
-	    }
+	public static void main(String[] args) throws Exception {
+		int port;
+		if (args.length > 0) {
+			port = Integer.parseInt(args[0]);
+		} else {
+			port = 8080;
+		}
+		new HttpHelloWorldServer(port).run();
+	}
 }
