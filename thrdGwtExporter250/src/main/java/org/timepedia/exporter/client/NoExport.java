6144afad4a65e0f43b11688bba73e237b1b21e93
mxro <mxro@nowhere.com>
2016-01-05 11:57:38 +1100

    Developing ObjectHub platform Autocommit Tue Jan 05 11:57:38 EST 2016

--- thrdGwtExporter250/src/main/java/org/timepedia/exporter/client/NoExport.java
+++ thrdGwtExporter250/src/main/java/org/timepedia/exporter/client/NoExport.java
@@ -1,5 +1,6 @@
 package org.timepedia.exporter.client;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,8 +10,9 @@ import java.lang.annotation.Target;
  * If a class is marked for export, used this method to selectively disable
  * certain methods or fields from being exported.
  */
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR})
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR })
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface NoExport {
 
 }
